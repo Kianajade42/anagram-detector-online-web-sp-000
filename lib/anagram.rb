941a21e1ae3d8d4692b9805cc37b5727637c4f5a
Kiana Long <kianajade42.kl@gmail.com>
2021-02-05 19:30:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+attr_accessor : matches 
+
+def initialize(words)
+  @matches=words 
+end 
+def same(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end 
+
+end 
+
+
+end
